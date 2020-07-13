f4bd432ee54655701d772ad82b2beb075c6c2b54
Erick Oliveira <erickcezar.oliveira@gmail.com>
2020-07-13 13:01:52 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,5 @@ export S3_URL="http://localhost:9000"
 export S3_ACCESS_KEY="AKIAIOSFODNN7"
 export S3_SECRET_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCY"
 
+pkill gunicorn
 gunicorn3 --bind 0.0.0.0:5000 wsgi:app > gunicorn.log &
